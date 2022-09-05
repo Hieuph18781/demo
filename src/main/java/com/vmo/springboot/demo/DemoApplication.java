c6f232b2ba44d769346c68ce611e0d6a92fe1d10
hieu <94371013+HieuThanh1212@users.noreply.github.com>
2022-09-05 15:13:08 +0700

    sds

--- src/main/java/com/vmo/springboot/demo/DemoApplication.java
+++ src/main/java/com/vmo/springboot/demo/DemoApplication.java
@@ -1,19 +1,17 @@
 package com.vmo.springboot.demo;
 
-import com.vmo.springboot.demo.Config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableWebMvc
-@EnableSwagger2
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
 //            new SwaggerConfig();
+
     }
 
 }
--- src/main/java/com/vmo/springboot/demo/SwaggerConfig.java
+++ src/main/java/com/vmo/springboot/demo/SwaggerConfig.java
@@ -1,7 +1,11 @@
-package com.vmo.springboot.demo.Config;
+package com.vmo.springboot.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,13 +17,25 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+@EnableWebMvc
+public class SwaggerConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry
+                .addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfoMetaData())
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.vmo.springboot.demo"))
                 .build();
     }
     private ApiInfo apiInfoMetaData() {
@@ -29,7 +45,7 @@ public class SwaggerConfig {
                 .contact(new Contact("Dev-Team", "https://www.hieu.com/", "veins1103@gmail.com"))
                 .license("Apache 2.0")
                 .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-                .version("1.0.0")
+                .version("1.1.0")
                 .build();
     }
 }
